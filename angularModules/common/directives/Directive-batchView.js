978deece86c9d74ca1d40c90d5dbbc1c0c5f7a04
Lijin <lijin@baabte.com>
2015-05-25 18:13:14 +0530

    Local build

---
+++ angularModules/common/directives/Directive-batchView.js
@@ -0,0 +1,105 @@
+angular.module('baabtra').directive('batchView',['$filter','$state', function($filter,$state) {
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+			batch:"=",
+			actionFlag:"=",
+			specificOption:"=",
+			shadow: "="
+		},
+		templateUrl: 'angularModules/common/directives/Directive-batchView.html',
+		link: function(scope, element, attrs, fn) {
+			scope.$watch('batch',function(){
+
+				scope.actions = [
+				/*{
+				"text": "<i class=\"fa fa-users\"></i>&nbsp;View mentees",
+				"click": "fnViewMentees()"
+				},*/
+				{
+				"text": "<i class=\"fa fa-hand-o-up\"></i>&nbsp;Mark attendance",
+				 "click": "fnMarkAttendance()"
+				},
+				{
+				"text": "<i class=\"fa fa-paperclip\"></i>&nbsp;Assign a course material",
+				"click": "fnAssignMaterial()"
+
+				},
+				{
+				"text": "<i class=\"fa fa-check\"></i>&nbsp;Evalute",
+				"click": "fnEvaluate()"
+
+				},
+				{
+				"text": "<i class=\"fa fa-bolt\"></i>&nbsp;View Results",
+				"click": "fnViewREsults()"
+				},
+				{
+				"text": "<i class=\"mdi-social-person-add\"></i>&nbsp;Allocate Evaluator",
+				"click": "fnAllocateEvaluator()"
+				},
+				{
+				"text": "<i class=\"fa fa-hand-o-up\"></i>&nbsp;Update attendance",
+				 "click": "fnEditAttendance()"
+				}
+				];
+				
+				/*if(scope.batch.batchMode=="onetime"){
+					scope.description = 'Starting date '+$filter('date')(scope.batch.startDate.$date)+'<br />Total joinings:'+scope.batch.totalJoining+'<br />End date: '+$filter('date')(scope.batch.endDate.$date);
+				}else{
+					var days=scope.batch.repeats.every;
+					scope.description = 'Starting date '+$filter('date')(scope.batch.startDate.$date)+'<br />Total joinings:'+scope.batch.totalJoining+'<br />Duration: Repeats each '+days+' months';
+				}*/
+
+				scope.dateConvertion=function(date){
+					return $filter('date')(date);
+				};
+
+
+				//function to add course materials
+				scope.fnAssignMaterial=function(){
+					$state.go("home.main.batchAssignment",{batchMappingId:scope.batch._id.$oid});
+				};
+
+				//function to load the mark the batch attendance 
+				scope.fnMarkAttendance=function(){
+					// $state.go("home.main.batchAttendance",{batchMappingId:scope.batch._id.$oid});
+					$state.go("home.main.markBatchAttendance",{batchMappingId:scope.batch._id.$oid,mode:0});
+				};
+
+				//function to load the mark the batch attendance 
+				scope.fnEditAttendance=function(){
+					// $state.go("home.main.batchAttendance",{batchMappingId:scope.batch._id.$oid});
+					$state.go("home.main.markBatchAttendance",{batchMappingId:scope.batch._id.$oid,mode:1});
+				};
+
+				//function to load evaluation partial based on batchMappingId
+				scope.fnEvaluate=function(){
+					$state.go("home.main.batchEvaluation",{batchMappingId:scope.batch._id.$oid});
+				};
+
+				//function to load evaluation partial based on batchMappingId
+
+				scope.fnViewREsults=function(){
+					$state.go("home.main.viewResults",{batchMappingId:scope.batch._id.$oid});
+				};
+				
+				scope.fnAllocateEvaluator=function(){
+					$state.go("home.main.allocateEvaluator",{batchMappingId:scope.batch._id.$oid});
+
+				};
+
+				// function for executing functions from name
+				scope.executeFunction=function (functionName) {
+					functionName=functionName.replace('()','');
+					scope[functionName]();
+				};
+
+
+				});
+
+
+		}
+	};
+}]);
---
+++ angularModules/course/partials/Partial-candidateCourseView.js
@@ -0,0 +1,21 @@
+angular.module('baabtra').controller('CandidatecourseviewCtrl',['$scope','$rootScope','candidateCourseView','commonService','$state',function($scope,$rootScope,candidateCourseView,commonService,$state){
+
+if(!$rootScope.userinfo){ //checking for the login credentilas is present or not
+      $rootScope.hide_when_root_empty=true;
+      commonService.GetUserCredentials($scope);
+}
+var courses = candidateCourseView.loadCoursesForCandidates($rootScope.userinfo.userLoginId);
+	courses.then(function (data) {
+		$scope.courses = angular.fromJson(JSON.parse(data.data));
+});
+
+$scope.navigateToDetails=function(courseId){
+	$state.go('home.main.CandidateCourseDetails',{courseId:courseId});
+	// $state.go('home.main.viewCourse',{courseId:courseId});
+};
+$scope.navigateToClassroom=function(courseMappingId){
+	$state.go('home.main.viewMyCourse',{courseMappingId:courseMappingId});
+};
+
+
+}]);
